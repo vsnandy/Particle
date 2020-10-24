538830777abc1f075fcd4aa4747636e15676274c
downhillskater91 <varuns.nandyal@gmail.com>
2020-10-24 18:04:57 -0500

    Get the Temp and Humidity every 1 second

--- TemperatureHumidity/src/TemperatureHumidity.ino
+++ TemperatureHumidity/src/TemperatureHumidity.ino
@@ -7,14 +7,32 @@
 
 #include "Grove_Temperature_And_Humidity_Sensor.h"
 
+DHT dht(D2);
+
+float temp, humidity;
+double temp_dbl, humidity_dbl;
+
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
+  Serial.begin(9600);
 
+  dht.begin();
+
+  Particle.variable("temp", temp_dbl);
+  Particle.variable("humidity", humidity_dbl);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
+  float temp, humidity;
 
+  temp = dht.getTempFarenheit();
+  humidity = dht.getHumidity();
+
+  temp_dbl = temp;
+  humidity_dbl = humidity;
+
+  delay(1000);
 }
