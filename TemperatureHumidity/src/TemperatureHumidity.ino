6afd2f0248f865ad104f0abcb5410ca5a87b6139
downhillskater91 <varuns.nandyal@gmail.com>
2020-10-24 16:33:16 -0500

    Adding TemperatureHumidity project

---
+++ TemperatureHumidity/src/TemperatureHumidity.ino
@@ -0,0 +1,20 @@
+/*
+ * Project TemperatureHumidity
+ * Description: Simple Project to get the Temperature and Humidity using Grove sensor
+ * Author: Varun Nandyal
+ * Date: 10/24/2020
+ */
+
+#include "Grove_Temperature_And_Humidity_Sensor.h"
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
